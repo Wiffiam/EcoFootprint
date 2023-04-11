f68a995457d870a9dbfb203e2440b88a5448d1a1
Wiffiam <wiffiam@wiffiam.com>
2023-04-10 22:02:47 -0400

    added comments

--- src/App.java
+++ src/App.java
@@ -9,21 +9,30 @@ public class App {
         System.out.print("\033[H\033[2J");  
         System.out.flush();  
     }
-    public static int findWattage(){
+    public static double findWattage(){
+        //initializes scanner and chrome webdriver
         Scanner input = new Scanner(System.in);
         WebDriver driver = new ChromeDriver();
+        //opens a window to the PCPartPicker url
         driver.get("https://pcpartpicker.com");
         System.out.println("A window has been opened for you to configure your current computer specifications.");
-        System.out.println("What is your PCPartPicker URL?");
+        System.out.println("What is your PCPartPicker build URL?");
         String url = input.nextLine();
+        //opens the users build url and pulls the css element corresponding to the pc's estimated wattage
         driver.get(url);
         String HTMLwattage = driver.findElement(By.cssSelector(".partlist__keyMetric")).getAttribute("innerHTML");
+        //trims the CSS element down to only contain the numerical value of the wattage followed by the W suffix
         String wattage = ((HTMLwattage.substring(HTMLwattage.indexOf("</span>"))).replace("</span>","").replace("</a>","")).trim();
+        //debug
+        System.out.println("How many hours do you use your PC per day?");
+        double usage = input.nextDouble();
+        int wattageNum = Integer.parseInt((wattage).replace("W","").trim());
+        double kgCarbon = 0.000433*usage*wattageNum;
         System.out.println("Your PC's estimated wattage: ");
         System.out.println(wattage);
-        int wattageNum = Integer.parseInt((wattage).replace("W","").trim());
+        System.out.println("Your PC's estimated carbon emissions per year: ");
         driver.quit();
-        return wattageNum;
+        return kgCarbon;
     }
     public static String [][] csvConversion() throws FileNotFoundException {
         Scanner readFile = new Scanner(new File("src/sorted_vehicles.csv"));
@@ -51,19 +60,27 @@ public class App {
         return csvFile;
     }
     public static double carCarbon() throws FileNotFoundException{
+        //initializes a scanner for the method to capture user inputs
         Scanner input = new Scanner(System.in);
+        //gets user's car information
         System.out.println("What model year is your car?");
         String carYear = input.nextLine();
         System.out.println("What make is your car?");
         String carMake = input.nextLine();
-        //converts km to miles for use in later calculations
         System.out.println("How many kilometers do you drive per year?");
+        //converts km to miles for use in later calculations
         double milesDriven = (input.nextInt())/1.609;
+
+        //calls upon the csv conversion method in order to get a 2D array containing all US car EPA fuel mileages since 1985
         String carDatabase[][] = csvConversion();
+        //creates a list to store all elements in the 2D array that match the user's model year and make
         List<Integer> modelRowIndexes = new ArrayList<Integer>();
         System.out.println("Select your vehicle from the following options:");
+        //initializes a counter to save the number of vehicles that match the user's search
         int counter = 0;
+        //linear search algorithm to find which elements in the 2D array match both the car make and car year
         for(int row=0;row<carDatabase.length;row++){
+            //column 46 corresponds to the make of the car while column 63 corresponds to the year in the CSV file
             if(carDatabase[row][46].equals(carMake)&&carDatabase[row][63].equals(carYear)){
                 counter++;
                 modelRowIndexes.add(row);
@@ -80,37 +97,26 @@ public class App {
     }
 
     public static void main(String[] args) throws FileNotFoundException{
-        boolean completedMenu = false;
         boolean exit = false;
         Scanner input = new Scanner(System.in);
         while(!exit){
+            clearScreen();
             System.out.println("Welcome to the Carbon Footprint calculator.");
             System.out.println("Please select a menu option:");
             System.out.println("a: Calculate your PC's carbon emissions");
             System.out.println("b: Calculate your car's carbon emissions");
             System.out.println("c: Calculate your home's carbon emissions");
-            if(completedMenu){
-                System.out.println("d: Get recommendations to reduce your carbon footprint");
-            }
-            //temporary comment
-            // String [][]converted = csvConversion();
-            
-            // System.out.println("enter column");
-            // int b = input.nextInt();
-            // System.out.println("enter row");
-            // int a = input.nextInt();
-            // System.out.println(converted[a][b]);
-            // input.nextLine();
+            System.out.println("d: Get recommendations to reduce your carbon footprint");
+            System.out.println("e: Save results to a text file");
+            System.out.println("Press any other key to exit");
 
             String menu_option = input.nextLine();
             double carbon = 0;
-            System.out.println(carbon);
             switch(menu_option){
                 case "a":
-                    int watts = findWattage();
+                    double watts = findWattage();
                     break;
                 case "b":
-                    System.out.println("Option B");
                     carbon += carCarbon();
                     break;
                 case "c":
@@ -123,7 +129,7 @@ public class App {
                     input.nextLine();
                     break;
                 default:
-                    System.out.println("goodbye");
+                    System.out.println("Goodbye");
                     exit = true;
                     break;
             }

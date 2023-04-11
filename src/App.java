4279275b02fa6b1af7ad1f4f443a6079f88b74db
Wiffiam <wiffiam@wiffiam.com>
2023-04-11 11:39:29 -0400

    updated household calculations

--- src/App.java
+++ src/App.java
@@ -81,7 +81,7 @@ public class App {
         //linear search algorithm to find which elements in the 2D array match both the car make and car year
         for(int row=0;row<carDatabase.length;row++){
             //column 46 corresponds to the make of the car while column 63 corresponds to the year in the CSV file
-            if(carDatabase[row][46].equals(carMake)&&carDatabase[row][63].equals(carYear)){
+            if(carDatabase[row][46].equalsIgnoreCase(carMake)&&carDatabase[row][63].equals(carYear)){
                 counter++;
                 modelRowIndexes.add(row);
                 System.out.println(counter+": "+carDatabase[row][47]);
@@ -95,15 +95,30 @@ public class App {
         System.out.println(kgCarbon);
         return kgCarbon;
     }
-    public static double homeCarbon(){
+    public static double homeCarbon(int count){
         Scanner input = new Scanner(System.in);
-        System.out.println("Test");
-        
+        double kgCarbon = 0;
+        if(count>=3){
+            return 0;
+        }else if(count == 0){
+            System.out.println("How many people are in your household, including you?");
+            int people = input.nextInt();
+            return homeCarbon(count+1)/people;
+        }else if(count == 1){
+            System.out.println("What is your monthly estimated natural gas bill?");
+            double gasBill = input.nextDouble();
+            return (gasBill * 60.78)+homeCarbon(count+1);
+        }else if(count == 2){
+            System.out.println("What is your monthly estimated electricity bill?");
+            double electricityBill = input.nextDouble();
+            return (electricityBill * 1.814)+homeCarbon(count+1);
+        }else{
+            return kgCarbon;
+        }
     }
 
     public static void main(String[] args) throws FileNotFoundException{
         //initializes 1D array to save the number of times the user has run through each menu option of the program. This will be useful when comparing the user's carbon emissions data
-        int[] calculationsPerformed = {0,0,0};
         double[] carbon = {0,0,0};
         double totalCarbon = 0;
         boolean exit = false;
@@ -123,15 +138,13 @@ public class App {
             switch(menu_option){
                 case "a":
                     carbon[0] = findWattage();
-                    calculationsPerformed[0]+=1;
                     break;
                 case "b":
                     carbon[1] = carCarbon();
-                    calculationsPerformed[1]+=1;
                     break;
                 case "c":
-                    carbon[2] = homeCarbon();
-                    calculationsPerformed[2]+=1;
+                    carbon[2] = homeCarbon(0);
+                    System.out.println(carbon[2]);
                     break;
                 case "d":
                     System.out.println("Option D");
@@ -145,10 +158,10 @@ public class App {
                     break;
                 case "f":
                     carbon[0]=carbon[1]=carbon[2] = 0;
-                    calculationsPerformed[0]=calculationsPerformed[1]=calculationsPerformed[2]=0;
                     System.out.println("Carbon emissions data has been reset");
                     System.out.println("Press any key to continue");
                     input.nextLine();
+                    break;
                 default:
                     System.out.println("Goodbye");
                     exit = true;
